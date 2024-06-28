b564dc98baa9ceed42bcb7d117e795bea04e086e
tarukansok <tarukansok@outlook.com>
2024-06-28 10:41:07 +0700

    Update run.js

--- run.js
+++ run.js
@@ -1,1 +1,12 @@
 
+(async()=>{
+
+  const waiting = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time));
+  };
+
+  for(let i=0;i< 10000000){
+    console.log("Ok")
+    await waiting(50000)
+  }
+})()
